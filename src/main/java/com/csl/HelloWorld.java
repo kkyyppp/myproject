cc1edcd6d9aff016c5e770668fd3ed31b6c67d61
csl <kkyyppp@gmail.com>
2019-02-21 11:22:58 +0800

    variable tutorial

--- src/main/java/com/csl/HelloWorld.java
+++ src/main/java/com/csl/HelloWorld.java
@@ -6,5 +6,15 @@ public class HelloWorld {
 
         Person p = new Person();
         p.hello();
+
+        int age =19;
+        Integer age2 = 19;
+        char c = 'a';
+        Character c2 = 'A';
+        byte b = 120;
+        float weight = 66.5f;
+        double height = 175;
+        boolean enroll = true;
+        String s ="str";
     }
 }
--- src/main/kotlin/com/csl/HelloWorld.kt
+++ src/main/kotlin/com/csl/HelloWorld.kt
@@ -5,6 +5,23 @@ fun main (args: Array<String>) {
         //Human().hello()
         val h = Human()
         h.hello()
+
+        //var: 值可改變
+        //val: 值不可改
+        //變數型態自動推斷 / 或自己加入資料型態
+        val v1 = 10
+        var v2 = 10.5
+        v2=9.5;
+
+        var i:Int = 10
+        var d:Double = 10.5
+        var b:Boolean = true
+        var f:Float = 10.1f
+        var s:String  = "str"
+        var c:Char = 'c'
+        var byte:Byte = 12
+
+
 }
 
 class Human() {
