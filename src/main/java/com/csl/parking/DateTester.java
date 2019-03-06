72dcfcd8b68a6717ffaf47708d3e0b2c0a98bc69
csl <kkyyppp@gmail.com>
2019-03-06 10:22:55 +0800

    第24章 java8時間新類別 LocalDateTime DateTimeFormatter Duration Instant

--- src/main/java/com/csl/parking/DateTester.java
+++ src/main/java/com/csl/parking/DateTester.java
@@ -2,11 +2,65 @@ package com.csl.parking;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 
 public class DateTester {
     public static void main(String[] args) {
+        java8();
+        java();
+    }
+
+
+    private static void java8() {
+
+        //Java8的新類別
+
+        //Instant
+        //預設時間格式是有帶時區的(沒指定為UTC+0) e.g.  2019-03-06T01:45:21.226Z
+        Instant instant = Instant.now();
+        System.out.println(instant);
+        System.out.println();
+
+        //不考慮時區 e.g. 2019-03-06T09:52:21.683 格式後面沒有"Z"
+        // LoaclDate=日期(年月日)   LoaclTIme=時間 LocalDateTime=日期+時間
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+        System.out.println();
+
+        //格式化方式
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        String customDate = now.format(formatter);
+        System.out.println(customDate);
+        System.out.println();
+
+        //將特定格式轉為時間
+        String customTime = "2019/03/01 12:00:00";
+        LocalDateTime time =  LocalDateTime.parse(customTime, formatter);
+        System.out.println(time);
+        System.out.println();
+
+        //時間設定
+        LocalDateTime other = LocalDateTime.of(2019,03,1,12,0,0);
+        System.out.println(other);
+        //時間運算
+        //特別注意 進行運算時 並不會更改原本的時間，而是輸出另一個時間(LocalDateTime 是 immutable)
+        LocalDateTime otherAfterPlus = other.plus(Duration.ofDays(10));
+        System.out.println(other);
+        System.out.println(otherAfterPlus);
+
+        other.minus(Duration.ofDays(10));
+        LocalDateTime otherAfterMinus = other.minus(Duration.ofDays(10));
+        System.out.println(other);
+        System.out.println(otherAfterMinus);
+
+    }
+
+    private static void java() {
         //取得當前時間
         Date date = new Date();
         System.out.println(date);
--- src/main/kotlin/com/csl/parking/DateTester.kt
+++ src/main/kotlin/com/csl/parking/DateTester.kt
@@ -1,34 +1,76 @@
 package com.csl.parking
 
 import java.text.SimpleDateFormat
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.Year
-import java.time.ZoneOffset
+import java.time.*
+import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoField
 import java.util.*
 
 fun main(args:Array<String>) {
+
+    java8()
+   // java()
+}
+
+fun java8() {
+    //Instant
+    //預設時間格式是有帶時區的(沒指定為UTC+0) e.g.  2019-03-06T01:45:21.226Z
+    val instant:Instant = Instant.now()
+    println(instant)
+    println()
+
+    //不考慮時區 e.g. 2019-03-06T09:52:21.683 格式後面沒有"Z"
+    //LoaclDate=日期(年月日)   LoaclTIme=時間 LocalDateTime=日期+時間
+    val now:LocalDateTime = LocalDateTime.now()
+    println(now)
+    println()
+
+    //格式化方式
+    val formatter:DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")
+    var customTime = now.format(formatter)
+    println(customTime)
+    println()
+
+    //將特定格式轉為時間
+    val customStr:String = "2019/03/01 12:00:00"
+    val time = LocalDateTime.parse(customStr, formatter)
+    println(time)
+    println()
+
+    //時間設定
+    val other:LocalDateTime = LocalDateTime.of(2019,3,1,12,0,0)
+    println(other)
+    //時間運算
+    //特別注意 進行運算時 並不會更改原本的時間，而是輸出另一個時間(LocalDateTime 是 immutable)
+    val otherAfterPlus = other.plus(Duration.ofDays(10))
+    println(other)
+    println(otherAfterPlus)
+    val otherAfterMinus = other.minus(Duration.ofDays(10))
+    println(other)
+    println(otherAfterMinus)
+}
+
+private fun java() {
     //取得當前時間
-    var date:Date = Date()
+    var date: Date = Date()
     println(date)
     println(date.time)
     println()
 
     //將時間轉為特定格式
-    val sdf:SimpleDateFormat = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
-    var custom:String = sdf.format(date)
+    val sdf: SimpleDateFormat = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+    var custom: String = sdf.format(date)
     println(custom)
     println()
 
     //將特定格式轉為時間
-    val customTime:String = "2019/03/01 12:00:00"
+    val customTime: String = "2019/03/01 12:00:00"
     println(sdf.parse(customTime))
     println()
 
     //calender 可以設定時間(為獨特別注意 月份起始是0 故4月要輸入3)
-    val calender:Calendar = Calendar.getInstance()
-    calender.set(2018,4,1,12,0,0)
+    val calender: Calendar = Calendar.getInstance()
+    calender.set(2018, 4, 1, 12, 0, 0)
     println(calender.time)
     calender.set(Calendar.YEAR, 2018)
     calender.set(Calendar.MONTH, 3)
@@ -39,7 +81,4 @@ fun main(args:Array<String>) {
     //calender作日期加法
     calender.add(Calendar.DAY_OF_YEAR, 10)
     println(calender.time)
-
-
-
 }
